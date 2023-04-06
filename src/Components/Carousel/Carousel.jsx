c310b28ea67bd24dd94df38e95ef6b3918635d63
Juan Rivera <107821359+RIVERA-PRO@users.noreply.github.com>
2023-04-06 20:45:28 -0300

    home- footer (#2)

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -15,6 +15,7 @@ import Carousel3 from '../../img/Carousel3.jpg';
 export default function Carousel() {
   return (
     <div className="container">
+
       <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -37,16 +38,27 @@ export default function Carousel() {
         className="swiper_container"
       >
         <SwiperSlide>
-          <img src={Carousel1} alt="Carousel1" />
+          <img src={Carousel1} alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Carousel2} alt="Carousel2" />
+          <img src={Carousel2} alt="slide_image" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Carousel3} alt="Carousel3" />
+          <img src={Carousel3} alt="slide_image" />
         </SwiperSlide>
 
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
       </Swiper>
     </div>
-  )
+  );
 }
+
---
+++ src/Pages/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FooterC from '../../Components/Footer/FooterC'
+
+export default function Footer() {
+    return (
+        <div>
+            <FooterC />
+
+        </div>
+    )
+}
