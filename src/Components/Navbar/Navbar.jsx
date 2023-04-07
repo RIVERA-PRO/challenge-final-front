764153e023854328df309c76462f8ce2510ef4af
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-04-06 21:00:23 -0300

    arreliyos

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -45,82 +45,82 @@ export default function Navbar() {
 
     return (
         <header>
-        <nav className={scrolled ? "navbar scrolled" : "navbar"}>
-            <div className='logo'>
-                <img src={Logo} alt="logo" />
-            </div>
-
-            <div className={`nav_items ${ isOpen && "open" }`} >
-                <div className="cerrar-nav" onClick={() => setIsOpen( !isOpen )}>
-                    x
-                </div>
-                <div className='logo-nav'>
+            <nav className={scrolled ? "navbar scrolled" : "navbar"}>
+                <div className='logo'>
                     <img src={Logo} alt="logo" />
-
-                </div>
-                <div className='iconos2'>
-                    <img src={Favourite} alt="logoFavourite" onClick={ handleModal } />
-                    <img src={Cart} alt="logoCart" onClick={ handleModalCart } />
-                    <img src={Profile} alt="logoUser" onClick={ handleModalUser } />
                 </div>
 
-                <div>
-                    <div className='enlaces'>
-                        <Anchor to={`/`} >Home</Anchor>
-                        <Anchor to={`/`} >Packages</Anchor>
-                        <Anchor to={`/`} >FAQ's</Anchor>
-                        {/* <Anchor to={`/`} >Conslt</Anchor> */}
+                <div className={`nav_items ${isOpen && "open"}`} >
+                    <div className="cerrar-nav" onClick={() => setIsOpen(!isOpen)}>
+                        x
+                    </div>
+                    <div className='logo-nav'>
+                        <img src={Logo} alt="logo" />
+
+                    </div>
+                    <div className='iconos2'>
+                        <img src={Favourite} alt="logoFavourite" onClick={handleModal} />
+                        <img src={Cart} alt="logoCart" onClick={handleModalCart} />
+                        <img src={Profile} alt="logoUser" onClick={handleModalUser} />
                     </div>
 
-                    <div class="redes-sociales">
-                        <Anchor to={`/`}><i class='fa fa-facebook'></i></Anchor>
-                        <Anchor to={`/`}><i class='fa fa-instagram'></i></Anchor>
-                        <Anchor to={`/`}> <i class='fa fa-linkedin'></i></Anchor>
-                        <Anchor to={`/`}><i class='fa fa-twitter'></i></Anchor>
-                        <Anchor to={`/`}> <i class='fa fa-whatsapp'></i></Anchor>
+                    <div>
+                        <div className='enlaces'>
+                            <Anchor to={`/`} >Home</Anchor>
+                            <Anchor to={`/`} >Packages</Anchor>
+                            <Anchor to={`/`} >FAQ's</Anchor>
+                            {/* <Anchor to={`/`} >Conslt</Anchor> */}
+                        </div>
+
+                        <div class="redes-sociales">
+                            <Anchor to={`/`}><i class='fa fa-facebook'></i></Anchor>
+                            <Anchor to={`/`}><i class='fa fa-instagram'></i></Anchor>
+                            <Anchor to={`/`}> <i class='fa fa-linkedin'></i></Anchor>
+                            <Anchor to={`/`}><i class='fa fa-twitter'></i></Anchor>
+                            <Anchor to={`/`}> <i class='fa fa-whatsapp'></i></Anchor>
+                        </div>
                     </div>
+
                 </div>
 
-            </div>
-
-            <div className='iconos'>
-                <img src={Favourite} alt="logoFavourite" onClick={ handleModal } />
-                <img src={Cart} alt="logoCart" onClick={ handleModalCart } />
-                <img src={Profile} alt="logoUser" onClick={ handleModalUser } />
-            </div>
-
-            <div className={`nav_toggle  ${ isOpen && "open" }`} onClick={() => setIsOpen( !isOpen )}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-
-            {modal && (
-                <div className="modal_content">
-                    <div className="modal-nav">
-                        <div className="cerrar-modal" onClick={ handleModal }>x</div>
-                        <h3>Favourite</h3>
-                    </div>
+                <div className='iconos'>
+                    <img src={Favourite} alt="logoFavourite" onClick={handleModal} />
+                    <img src={Cart} alt="logoCart" onClick={handleModalCart} />
+                    <img src={Profile} alt="logoUser" onClick={handleModalUser} />
                 </div>
-            )}
-            {modalCart && (
-                <div className="modal_content">
-                    <div className="modal-nav">
-                        <div className="cerrar-modal" onClick={ handleModalCart }>x</div>
-                        <h3>Cart</h3>
-                    </div>
-                </div>
-            )}
-            {modalUser && (
-                <div className="modal_content">
-                    <div className="modal-nav">
-                        <div className="cerrar-modal" onClick={ handleModalUser }>x</div>
-                        <h3>Your Profile</h3>
-                    </div>
-                </div>
-            )}
 
-        </nav>
+                <div className={`nav_toggle  ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+                {modal && (
+                    <div className="modal_content">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModal}>x</div>
+                            <h3>Favourite</h3>
+                        </div>
+                    </div>
+                )}
+                {modalCart && (
+                    <div className="modal_content">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModalCart}>x</div>
+                            <h3>Cart</h3>
+                        </div>
+                    </div>
+                )}
+                {modalUser && (
+                    <div className="modal_content">
+                        <div className="modal-nav">
+                            <div className="cerrar-modal" onClick={handleModalUser}>x</div>
+                            <h3>Your Profile</h3>
+                        </div>
+                    </div>
+                )}
+
+            </nav>
 
         </header>
     )
